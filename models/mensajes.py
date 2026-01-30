48cbeb3a9b9e1ad9d13758a4f5e91e64657c06dc
Carlos Caletti <35155625+Studio-Caletti@users.noreply.github.com>
2026-01-30 11:47:00 -0600

    Delete mensajes.py

--- models/mensajes.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class HolaMundoMensaje(models.Model):
-    _name = 'hola.mundo.mensaje'
-    _description = 'Tabla de Mensajes de Carlos'
-
-    name = fields.Char(string='Título', required=True)
-    contenido = fields.Text(string='Mensaje')
-    fecha = fields.Date(string='Fecha de Creación', default=fields.Date.today)
